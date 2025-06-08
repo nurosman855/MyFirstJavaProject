21d38b72046df9bff01a9bf81b73fcb86ec19b07
nurosman855 <nurosman855@gmail.com>
2025-06-08 13:53:16 +0200

    Ändra order till ordet och lagt till ordet "Stop"

--- src/inlämning/TextProgram.java
+++ src/inlämning/TextProgram.java
@@ -11,7 +11,7 @@ public class TextProgram {
         String rad;
 
         while(true) { // Startar en evig loop
-            System.out.println("Skriv in text (Avsluta med order ): ");
+            System.out.println("Skriv in text (Avsluta med ordet Stop ): ");
             rad = scan.nextLine(); // Läser in en rad text
             if (rad.equalsIgnoreCase("stop")) { ///  kolla när användare skriver stop.
                 break; // avslut loopen.
